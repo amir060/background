b1c353187e9d51a5ff530bbde74dc9ba4fe49370
Amir Madawi <amadaoui21@gmail.com>
2019-10-31 17:10:40 -0700

    new project was opened

---
+++ functions/handlers/screams.js
@@ -0,0 +1,82 @@
+const { db } = require('../util/admin');
+
+exports.getAllScreams = (req, res) => {
+  db.collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+          commentCount: doc.data().commentCount,
+          likeCount: doc.data().likeCount,
+          userImage: doc.data().userImage
+        });
+      });
+      return res.json(screams);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
+};
+
+exports.postOneScream = (req, res) => {
+  if (req.body.body.trim() === '') {
+    return res.status(400).json({ body: 'Body must not be empty' });
+  }
+
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.user.handle,
+    userImage: req.user.imageUrl,
+    createdAt: new Date().toISOString(),
+    likeCount: 0,
+    commentCount: 0
+  };
+
+  db.collection('screams')
+    .add(newScream)
+    .then((doc) => {
+      const resScream = newScream;
+      resScream.screamId = doc.id;
+      res.json(resScream);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'something went wrong' });
+      console.error(err);
+    });
+};
+// Fetch one scream
+exports.getScream = (req, res) => {
+  let screamData = {};
+  db.doc(`/screams/${req.params.screamId}`)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: 'Scream not found' });
+      }
+      screamData = doc.data();
+      screamData.screamId = doc.id;
+      return db
+        .collection('comments')
+        .orderBy('createdAt', 'desc')
+        .where('screamId', '==', req.params.screamId)
+        .get();
+    })
+    .then((data) => {
+      screamData.comments = [];
+      data.forEach((doc) => {
+        screamData.comments.push(doc.data());
+      });
+      return res.json(screamData);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
+};
---
+++ functions/util/firebaseConfig.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+  apiKey: "AIzaSyBsGeto2YaZ_cMOxX26gBCTr1ryULbfAD8",
+    authDomain: "myapp-cc701.firebaseapp.com",
+    databaseURL: "https://myapp-cc701.firebaseio.com",
+    projectId: "myapp-cc701",
+    storageBucket: "myapp-cc701.appspot.com",
+    messagingSenderId: "444738559580",
+    appId: "1:444738559580:web:413b595a8a1ebe99d1d099",
+    measurementId: "G-D8P1WXVBR5"
+};
